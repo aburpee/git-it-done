9ea04e3b6b2d2a1be00d177d7c6cb4a70db2c95d
bonobo <arburpee@gmail.com>
2022-03-02 18:59:29 -0800

    added query parameters and error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,29 @@
 var issueContainerEl = document.querySelector('#issues-container');
+var limitWarningEl = document.querySelector('#limit-warning');
+var repoNameEl = document.querySelector('#repo-name');
 
 
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split('=')[1];
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace('./index.html')
+    }
+};
+
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = 'To see more than 30 issues, visit ';
+
+    var linkEl = document.createElement('a');
+    linkEl.textContent = 'See more issues on Github.com';
+    linkEl.setAttribute('href', 'https://github.com/' + repo + '/issues');
+    linkEl.setAttribute('target', '_blank');
+
+    limitWarningEl.appendChild(linkEl);
+};
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -10,9 +33,12 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+                if (response.headers.get('Link')) {
+                    displayWarning(repo);
+                }
             });
         } else {
-            alert('there was a problem with your request')
+            document.location.replace('./index.html')
         }
     })
 };
@@ -47,4 +73,5 @@ var displayIssues = function(issues) {
     }
 }
 
-getRepoIssues('facebook/react');
+getRepoName();
+// getRepoIssues();
